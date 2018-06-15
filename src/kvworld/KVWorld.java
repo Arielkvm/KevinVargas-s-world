0421d5e707b05f6dba43813a7c5a05613cf0d432
Kevin Vargas <00139917>
2018-06-14 19:40:17 -0600

    very ok

--- src/kvworld/KVWorld.java
+++ src/kvworld/KVWorld.java
@@ -1,9 +1,14 @@
 package kvworld;
 
+import kvworld.Menus.Menus;
+
 public class KVWorld {
 
     public static void main(String[] args) {
-       
+    	Menus m = new Menus();
+    	
+    	m.MenuInicio1(); 	
+    	m.Menu2();
     }
     
 }
